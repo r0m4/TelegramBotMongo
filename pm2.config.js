18d6aa453d31c4bed4e86a57cec018dd1fd26244
Zhylinskyi Roman <formicrotech@gmail.com>
2024-03-28 05:28:38 +0200

    changed Path Nodemon

--- pm2.config.js
+++ pm2.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "telegrambot",
-      script: "nodemon --exitcrash index.js", // Укажите здесь имя вашего основного файла
+      script: "/home/ubuntu/.nvm/versions/node/v21.7.1/bin/nodemon --exitcrash index.js", // Укажите здесь имя вашего основного файла
       cwd: "/home/ubuntu/UKGBot/TelegramBotMongo", // Укажите путь к вашему приложению
       watch: true, // Если вы хотите, чтобы pm2 отслеживал изменения файлов и перезапускал приложение при их изменении
       ignore_watch: ["node_modules", "logs"] // Игнорируемые папки      
