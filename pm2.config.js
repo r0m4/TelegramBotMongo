a9790d0b576f1630b39ab0e7f9c2db408fc3aee5
Zhylinskyi Roman <formicrotech@gmail.com>
2024-03-28 04:34:03 +0200

    pm2.config.js fixed

--- pm2.config.js
+++ pm2.config.js
@@ -7,8 +7,8 @@ module.exports = {
       watch: true, // Если вы хотите, чтобы pm2 отслеживал изменения файлов и перезапускал приложение при их изменении
       ignore_watch: ["node_modules", "logs"], // Игнорируемые папки
       log_date_format: "YYYY-MM-DD HH:mm:ss.SSS", // Формат даты в логах
-      out_file: "./logs/pm2_out.log", // Путь к файлу с выводом stdout
-      error_file: "./logs/pm2_error.log", // Путь к файлу с выводом stderr
+      out_file: '/home/ubuntu/UKGBot/TelegramBotMongo/logs/pm2_out.log', // Путь к файлу с выводом stdout
+      error_file: '/home/ubuntu/UKGBot/TelegramBotMongo/logs/pm2_error.log', // Путь к файлу с выводом stderr
       merge_logs: true // Объединять логи от всех экземпляров приложения в один файл
     }
   ]
