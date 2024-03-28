f7e907460625c90816e15d16ad051e009904d4cd
Zhylinskyi Roman <formicrotech@gmail.com>
2024-03-28 03:21:10 +0200

    fixed bags with logging

--- indexIntemedia.js
+++
@@ -1,7 +0,0 @@
-const TelegramApi = require('node-telegram-bot-api');
-const token = '6827132012:AAGMXbGK89_4Xe2-qaB_ARPM_qJ-FIZdIX8';
-const bot = new TelegramApi(token, {polling:true});
-const Json2CsvParser = require("json2csv").Parser;
-const fs = require("fs");
-const path = require("path");
-
