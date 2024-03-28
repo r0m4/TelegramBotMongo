174977e049db6a7656419003c5f794aa7f4a56dd
Zhylinskyi Roman <formicrotech@gmail.com>
2024-03-28 04:37:10 +0200

    turned off pm2 logs

--- pm2.config.js
+++ pm2.config.js
@@ -5,11 +5,7 @@ module.exports = {
       script: "nodemon --exitcrash index.js", // Укажите здесь имя вашего основного файла
       cwd: "/home/ubuntu/UKGBot/TelegramBotMongo", // Укажите путь к вашему приложению
       watch: true, // Если вы хотите, чтобы pm2 отслеживал изменения файлов и перезапускал приложение при их изменении
-      ignore_watch: ["node_modules", "logs"], // Игнорируемые папки
-      log_date_format: "YYYY-MM-DD HH:mm:ss.SSS", // Формат даты в логах
-      out_file: '/home/ubuntu/UKGBot/TelegramBotMongo/logs/pm2_out.log', // Путь к файлу с выводом stdout
-      error_file: '/home/ubuntu/UKGBot/TelegramBotMongo/logs/pm2_error.log', // Путь к файлу с выводом stderr
-      merge_logs: true // Объединять логи от всех экземпляров приложения в один файл
+      ignore_watch: ["node_modules", "logs"] // Игнорируемые папки      
     }
   ]
 };
